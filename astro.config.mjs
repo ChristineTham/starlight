4a3c0fd640b624f27c8df4ef5ab45a5723defc10
Christine Tham <chris.tham@hellotham.com>
2025-01-14 18:16:54 +1100

    Basic customisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,45 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tipitaka-md.github.io',
-	integrations: [
-		starlight({
-			title: 'Tipitaka',
-			social: {
-				github: 'https://github.com/ChristineTham/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://tipitaka-md.github.io",
+  integrations: [
+    starlight({
+      title: "Tipitaka",
+      social: {
+        github: "https://github.com/ChristineTham/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: [
+        "./src/styles/custom.css",
+        "@fontsource/noto-sans/400.css",
+        "@fontsource/noto-sans/600.css",
+      ],
+      logo: {
+        src: "./src/assets/rosely.svg",
+      },
+	  defaultLocale: 'root',
+      locales: {
+        root: {
+          label: 'Pāḷi',
+          lang: 'pli',
+        },
+        'en': {
+          label: 'English',
+          lang: 'en',
+        },
+      },
+    }),
+  ],
 });
